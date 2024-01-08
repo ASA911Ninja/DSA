a28bc859958e9649328091577e3d38f760b74a1b
Arya911 <64189431+Arya911@users.noreply.github.com>
2024-01-08 18:38:04 +0530

    Create DiameterOfTree.java

---
+++ DSA_JAVA/BinaryTrees/DiameterOfTree.java
@@ -0,0 +1,22 @@
+package BinaryTrees;
+
+// diameter of the tree is the length of the longest path between 2 leaf nodes
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class DiameterOfTree {
+    public static void main(String[] args) {
+        
+    }
+    
+
+}
